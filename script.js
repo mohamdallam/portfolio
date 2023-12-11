7190c4e7149e2a549c30b811c7e42bae136fcff9
mohamdallam <mohamedahmedallam1@gmail.com>
2023-12-11 20:54:07 +0200

    INIT

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Use Masonry.js to create dynamic layout containing cards of varying heights */
+document.addEventListener("DOMContentLoaded", function () {
+  const grid = document.querySelector("main");
+  const masonry = new Masonry(grid, {
+    itemSelector: ".card",
+    columnWidth: ".card",
+    gutter: 16,
+    fitWidth: true,
+    stagger: 30,
+    isAnimated: true,
+  });
+
+  masonry.reloadItems();
+
+  imagesLoaded(grid).on("always", function () {
+    masonry.layout();
+  });
+});
